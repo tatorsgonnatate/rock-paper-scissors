6edfc84cad0e46bc217f4e42e63502ab33e4951e
tatorsgonnatate <123611189+tatorsgonnatate@users.noreply.github.com>
2023-07-28 16:45:12 -0500

    Finsih testing and logic on game, done for now until I finishing styling.

--- main.js
+++ main.js
@@ -1,74 +1,68 @@
-playerScore = 0;
-computerScore = 0; 
-//Computer Choices
+let playerScore = 0;
+let computerScore = 0;
+
+// Computer Choices
 let choice = ['rock', 'paper', 'scissors'];
-//Getting Computer to select random choice
+
+// Getting Computer to select random choice
 function getComputerChoice() {
-    return (choice[(Math.floor(Math.random() * choice.length))]);
+    return choice[Math.floor(Math.random() * choice.length)];
 }
 
-//Buuton clicked with choice and playing round
+// Button clicked with choice and playing round
 let selectedValue = '';
 let result = '';
-const buttons = document.querySelectorAll('.game-button');
+let gameResult = '';
 
-buttons.forEach(function (i){
-   i.addEventListener('click', function() {
-       selectedValue = this.value;
-       playRound()
-       console.log(result);  
-   });
+const buttons = document.querySelectorAll('.game-button');
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        if (playerScore !== 5 && computerScore !== 5) {
+            selectedValue = this.value;
+            playRound();
+        }
+    });
 });
 
-//Give player Round Winner
-//result = document.getElementById('round-winner').innerHTML
-
-
-
 // Play round and return who wins
-function playRound(playerSelection, computerSelection){
-    playerSelection = selectedValue;
-    computerSelection = getComputerChoice();
-    //document.getElementById('round-winner').textContent = `${result}`
+function playRound() {
+    const playerSelection = selectedValue;
+    const computerSelection = getComputerChoice();
 
-        if (playerSelection === computerSelection){
-           return result = document.getElementById('round-winner').textContent = ('Tie! No winner this round.')
-        }else if((playerSelection === 'rock' && computerSelection === 'paper')
-        ||(playerSelection === 'paper' && computerSelection === 'scissors')
-        ||(playerSelection === 'scissors' && computerSelection === 'rock')){
-            if (computerScore <= 5) {
-                computerScore++;
-            }
-            return result = document.getElementById('round-winner').textContent = (`You lose ${playerSelection} beats ${computerSelection}`)
-        }else if((playerSelection === 'paper' && computerSelection === 'rock')
-        ||(playerSelection === 'scissors' && computerSelection === 'paper')
-        ||(playerSelection === 'rock' && computerSelection === 'scissors')){
-            if (playerScore <=5) {
-                playerScore++;
-            }
-            return result =document.getElementById('round-winner').textContent = (`You win ${playerSelection} beats ${computerSelection}`)  
-        }else {
-            return result = document.getElementById('round-winner').textContent = ('That is not a valid selection')
+    if (playerSelection === computerSelection) {
+        result = document.getElementById('round-winner').textContent = 'Tie! No winner this round.';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'rock')
+    ) {
+        if (computerScore < 5 && playerScore !== 5) {
+            computerScore++;
         }
-        
+        result = document.getElementById('round-winner').textContent = `You lose ${playerSelection} beats ${computerSelection}`;
+    } else if (
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'rock' && computerSelection === 'scissors')
+    ) {
+        if (playerScore < 5 && computerScore !== 5) {
+            playerScore++;
+        }
+        result = document.getElementById('round-winner').textContent = `You win ${playerSelection} beats ${computerSelection}`;
     }
-    
-//Play 5 rounds of rps and keep score and give winner at the end of the game
 
-/*let n = ''
-const game = () => {
-    let i =1;
+    // Update the score display
+    document.getElementById('score').innerText = `Your Score: ${playerScore} Computer Score: ${computerScore}`;
 
-    while (i <= 5) {
-        console.log(playRound())
-        i++
+    // Check if the game is over
+    if (playerScore === 5 || computerScore === 5) {
+        gameResult = document.getElementById('game-over').textContent = 'Play Again?';
     }
-    if (playerScore > computerScore) {
-        return `You beat the computer by ${playerScore - computerScore}. Great Job!`
-    }else if (computerScore > playerScore) {
-        return `The Computer beat you by ${computerScore - playerScore}. The machines are taking over!`
-    }else {
-        return "You both lose, life ain't fair and the world is mean!" 
+
+    // Check and display the final winner
+    if (playerScore === 5) {
+        gameResult = document.getElementById('game-winner').textContent = `You beat the computer by ${playerScore - computerScore}. Great Job!`;
+    } else if (computerScore === 5) {
+        gameResult = document.getElementById('game-winner').textContent = `The Computer beat you by ${computerScore - playerScore}. The machines are taking over!`;
     }
 }
-*/
